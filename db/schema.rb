4c6a9c502432beb8e4a516e514896863c19fab04
theInternauts <cricketw@wearetheinternauts.com>
2014-03-11 01:54:31 -0700

    added a JOIN table for Game and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311083114) do
+ActiveRecord::Schema.define(version: 20140311084746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20140311083114) do
     t.string   "game_score"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "games_users", id: false, force: true do |t|
+    t.integer "game_id", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: true do |t|
