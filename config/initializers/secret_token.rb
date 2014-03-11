f6de3fbed3f7dd9622276e748a71638ad6ca745e
theInternauts <cricketw@wearetheinternauts.com>
2014-03-10 23:00:43 -0700

    initial rails app plus db setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoGame::Application.config.secret_key_base = '378d3ccda68cc6921579bb075022624fc0d94187e8e17bdcb13ad1f0b177de8a7d06ab1f589086fc27a06cbf672db168dc8f597e4d044f8a2d82b4283695e5a5'
